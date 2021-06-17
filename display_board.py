8cb7648d112c707af0b48aae0e61c08cfd99631e
Lorenzo Galli <galli.lorenzo.x@gmail.com>
2021-06-17 12:54:59 +0200

    Fix problem with the 2d list

--- display_board.py
+++ display_board.py
@@ -10,7 +10,8 @@ back_color = '#1d95ad'
 # Measurement
 size_x = 600
 size_y = 600
-sqr_side = size_x/9
+square_num = 9
+sqr_side = size_x/square_num
 
 # Font 
 chara = pygame.font.Font('freesansbold.ttf', 40)
@@ -22,26 +23,21 @@ pygame.display.set_caption('Sudoku :3')
 screen = pygame.display.set_mode((size_x, size_y))
 screen.fill(back_color)
 
-# This is to divide the squares  !! TO DO !!
-square_num = 9
-board = []
-print(board)
-par_list = []
+# This is to divide the squares 
+board, par_list = [], []
 for i in range(square_num):  # i == y-coordinate
     for j in range(square_num): # j == x-coordinate
+
+        # create a square that take the whole tile and append it to a temporary list
         par_list.append(pygame.Rect(size_x*j/9, size_x*i/9, sqr_side, sqr_side))
-    board.append(par_list)
-board = [board[-1]]
-print(board)
+    board.append(par_list)  # append the t. list to the final list
+    par_list = [] # empty the par list
 
 text = chara.render('1', True, line_col)
-# txt_surf = font.render(str(), True, font_color)
-textRect = text.get_rect(center=(50, 50))
-screen.blit(text, textRect)
 
 # Draws board and makes line thicker if it's one of the two main lines
-for i in range(0, 10):
-    line_div = size_x*i/9
+for i in range(square_num):
+    line_div = size_x*i/square_num
     if i % 3 == 0:
         width = 5
     else:
@@ -61,8 +57,9 @@ def main():
             # This is how a player select the square to mark
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
-                for square in board:
-                    for tile in square:
+                for square in board:  # here we are itering through the rows
+                    for tile in square:  # here through the elements
+                        
                         if tile.collidepoint(pos):
                             screen.blit(text, tile)
             
