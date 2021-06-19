e8e36a70401bd9798e820578c3fb9eb40702d89a
Lorenzo Galli <galli.lorenzo.x@gmail.com>
2021-06-19 17:59:14 +0200

    Arrow keys + exception for keyboard input

--- display_board.py
+++ display_board.py
@@ -28,6 +28,7 @@ screen = pygame.display.set_mode((size_x, size_y))
 screen.fill(back_color)
  
 
+# This is the class of the tiles of our 9x9 grid
 class tile:
     def __init__(self, row, col, side, total_rows):
         self.row = row
@@ -37,19 +38,18 @@ class tile:
         self.side = side
         self.rect = (self.x, self.y, self.side, self.side)
         self.total_rows = total_rows
-        self.text = text
+        self.text = ''
         self.selected = False
 
+    # Selects the tile
     def select(self):
         pygame.draw.rect(screen, sel_sqr_col, self.rect, 0)
         self.selected = True
 
+    # Deselects the tile
     def deselect(self):
         pygame.draw.rect(screen, back_color, self.rect, 0)
         self.selected = False
-
-
-
 
 
 # This is to divide the squares 
@@ -66,6 +66,7 @@ def make_board():
         par_list = [] # empty the par list
     return board
 
+
 # Draws board and makes line thicker if it's one of the two main lines
 def draw_board():
     for i in range(square_num):
@@ -79,7 +80,6 @@ def draw_board():
 
 board = make_board()
 
-# this function checks if a square is selected and deselects it
 def deselect_all_tiles():
     for col in board:
         for tile in col:
@@ -88,46 +88,103 @@ def deselect_all_tiles():
     return board
 
 
+# We return the marked square if any
+def get_marked_square():
+    for col in board:
+        for tile in col:
+            if tile.selected == True:
+                return tile
+
+
 # main loop
 def main():
-    running = True
-    pressed_key = ''
+
     board = make_board()
+    
+    running = True
     while running:
         FPS = 60  # Frame rate
         clock = pygame.time.Clock()
         clock.tick(FPS)
 
+        # This captures every event in pygame
         for event in pygame.event.get():
             
             # This is how a player select the square to mark
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
-                board = deselect_all_tiles()
+                board = deselect_all_tiles()  # if a user pressed with the mouse deselect all
+                
                 for col in board:  # here we are itering through the rows
                     for tile in col:  # here through the elements
-                        tile_check = pygame.Rect(tile.rect)
-                        if tile_check.collidepoint(pos):
+                        tile_check = pygame.Rect(tile.rect) # we create a rectangle
+                        
+                        if tile_check.collidepoint(pos):  # if mouse touches tile
+                            tile.select()  # we select the tile
+
+
+            # Here we take the keyboard input and tranform them into outputs
+            if event.type == KEYDOWN:
+                pressed_key = ''
+                tile_to_write = get_marked_square()
+
+                # We remove the entire array if the backspace is pressed
+                if event.key == K_BACKSPACE:
+                    pressed_key = []
+                # This allows the player to type one letter max and only num from 1 to 9
+                elif event.key in [K_1, K_2, K_2, K_3, K_4, K_5, K_6, K_7, K_8, K_9] and len(pressed_key) == 0:
+                    pressed_key += event.unicode 
+                    print(pressed_key)
+
+                # Here we check if it's a movement key
+                if event.key in [K_DOWN, K_UP, K_RIGHT, K_LEFT, K_s, K_d, K_a, K_w]: 
+
+                    # We get the last tile to understand where to move and we deselect it 
+                    last_tile = get_marked_square()
+                    board = deselect_all_tiles()
+
+                    try:
+                        # MOVE DOWN
+                        if event.key == K_DOWN or event.key == K_s:
+                            if last_tile.col > 7:  # if we go too right or down it will throw an index error
+                                last_tile.col = -1  # so we need to resect the index to 0
+                            tile = board[last_tile.col + 1][last_tile.row]
                             tile.select()
 
+                        # MOVE UP
+                        elif event.key == K_UP or event.key == K_w:
+                            tile = board[last_tile.col - 1][last_tile.row]
+                            tile.select()
 
-            # This allows the player to type one letter max and only num from 1 to 9
-            if event.type == KEYDOWN:
-                if event.key == K_BACKSPACE:
-                    pressed_key = pressed_key[:-1]
-                elif event.key in [K_1, K_2, K_2, K_3, K_4, K_5, K_6, K_7, K_8, K_9] and len(pressed_key) == 0:
-                    pressed_key += event.unicode
-                print(pressed_key)
+                        # MOVE RIGHT
+                        elif event.key == K_RIGHT or event.key == K_d:
+                            if last_tile.row > 7:
+                                last_tile.row = -1
+                            tile = board[last_tile.col][last_tile.row + 1]
+                            tile.select()
+
+                        # MOVE LEFT 
+                        elif event.key == K_LEFT or event.key == K_a:
+                            tile = board[last_tile.col][last_tile.row - 1]
+                            tile.select()
+                    
+                    # if the user uses mov keys without having a previous tile it
+                    # would throw an error so we use except to catch it
+                    except AttributeError as e:
+                        pass
 
 
             # This is to quit the game and make sure it doesn't loop forever
             if event.type == pygame.QUIT:
                 running = False
         
+
+        # We need to draw our board every frame
         draw_board()
 
         # This is to make sure the differences are updated and displayed
         pygame.display.update()
+    
     pygame.quit()
     quit()
 
