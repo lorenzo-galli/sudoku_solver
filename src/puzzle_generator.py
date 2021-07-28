ba1320b6243c304dfa1f99cc0521d8870e4555b4
lorenzo-galli <galli.lorenzo.x@gmail.com>
2021-07-28 17:40:58 +0200

    Just to backup

--- src/puzzle_generator.py
+++ src/puzzle_generator.py
@@ -1,6 +1,7 @@
 from random import randint
 from values import square_num
 from json import dumps, loads
+from math import floor
 
 boardlink = f'boards/{square_num}x{square_num}.json'
 
@@ -21,8 +22,12 @@ list_of_puzzles = read_json(boardlink)
 num_of_puzzles = len(list_of_puzzles)
 random_num = randint(1, num_of_puzzles-1)
 
+random_num = 2
+
 initial_board = list_of_puzzles[random_num]
 empty_board = list_of_puzzles[0]
+
+initial_board = [['', '5', '', '6', '7', '8', '', '', ''], ['6', '7', '', '', '9', '5', '3', '', ''], ['1', '', '8', '3', '', '2', '5', '', '7'], ['8', '', '9', '7', '', '1', '4', '2', ''], ['', '', '', '', '', '3', '7', '9', '1'], ['', '', '', '4', '2', '', '8', '', '6'], ['9', '6', '1', '5', '3', '', '2', '8', ''], ['2', '8', '', '9', '1', '', '6', '5', '3'], ['5', '4', '3', '2', '8', '', '1', '7', '9']]
 
 
 # Rotates the board clockwise of x degrees
@@ -33,7 +38,7 @@ def rotate_board(to_rotate: list, degrees: int, swap_rows_cols):
     # we fill the board with placeholders
     for i in range(square_num):
         for j in range(square_num):
-            cols.append('')
+            cols.append("")
         rotated.append(cols)
         cols = []
 
@@ -57,12 +62,62 @@ def rotate_board(to_rotate: list, degrees: int, swap_rows_cols):
     return rotated
 
 
-def create_new_puzzles(compl_board: list[list], solve): 
-    solvable = True
-    while solvable:
+def create_new_puzzles(board, solve):
+    solve(True)
+    solvable = False
+
+    for i in range(floor(square_num ** 2 / 2)):
+        ran_col = randint(0, 8)
+        ran_row = randint(0, 8)
+        board[ran_col][ran_row].text = ""
+
+
+    while not solvable:
         ran_col = randint(0, 8)
         ran_row = randint(0, 8)
 
-        compl_board[ran_col][ran_row].text = ''
-        solvable = solve()
+        board[ran_col][ran_row].text = ""
+        solved = solve(True, True)
+        if solved:
+            solvable = True
 
+
+def create_new_puzzles(board):
+    initial_board = board
+    solve(True, False)
+    solvable = True
+
+    for i in range(floor(square_num ** 2 / 2)):
+        ran_col = randint(0, 8)
+        ran_row = randint(0, 8)
+        if board[ran_col][ran_row].text == '':
+            i -= 1
+        board[ran_col][ran_row].text = ''
+
+    print(backboard(board))
+    print(solve(True, True))
+
+
+    for j in range(20):
+        # if not solvable:
+        #     board = initial_board
+        #     print(backboard(board))
+        #     return solvable
+        #     break
+        ran_col = randint(0, 8)
+        ran_row = randint(0, 8)
+        if board[ran_col][ran_row].text == '':
+            j -= 3
+        board[ran_col][ran_row].text = ""
+        initial_board = board
+
+
+        # solvable = solve(True, True)
+        board = initial_board
+        print(backboard(board))
+        return solvable
+    
+
+
+
+print(create_new_puzzles(board))
